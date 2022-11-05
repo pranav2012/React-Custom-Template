6c7235d7b3be0ebf418cda29547670d0a5fe976d
pranav2012 <p2012agarwal@gmail.com>
2022-11-05 23:19:46 +0530

    fix: initial state

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,7 +14,25 @@ function App() {
 
   const [data, setdata] = useState([]);
   const [activeType, setActiveType] = useState("All");
-  const [chartData, setChartData] = useState({});
+  const [chartData, setChartData] = useState({
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: 
+    [
+      {
+        label: "First dataset",
+        data: [33, 53, 85, 41, 44, 65],
+        fill: true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)"
+      },
+      {
+        label: "Second dataset",
+        data: [33, 25, 35, 51, 54, 76],
+        fill: false,
+        borderColor: "#742774"
+      }
+    ]
+  });
 
   const fetchData = async () => {
     const res = await fetch('http://fetest.pangeatech.net/data');
